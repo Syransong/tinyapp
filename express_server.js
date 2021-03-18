6ecf1745c248f1d3745a2dd07cc851d93fcaeca5
Karina Song <ksysong@gmail.com>
2021-03-18 04:34:07 +0000

    refactored urlDatabase object so each shortURL has a longURL and userID

--- express_server.js
+++ express_server.js
@@ -11,8 +11,14 @@ app.set("view engine", "ejs");
 
 //Global Objects
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  "b2xVn2": {
+    longURL: "http://www.lighthouselabs.ca",
+    userID: "kirby"
+  },
+  "9sm5xK": {
+    longURL: "http://www.google.com",
+    userID: "kirby"
+  }
 };
 
 const users = {
