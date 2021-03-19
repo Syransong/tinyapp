5ea88c95a31cd9c106f1e6cdb9a74b09bb28a459
Karina Song <ksysong@gmail.com>
2021-03-19 04:36:17 +0000

    added test to see if it return undefined if a nonexistent email was entered

--- test/helpersTest.js
+++ test/helpersTest.js
@@ -17,10 +17,16 @@ const testUsers = {
 
 describe('getUserByEmail', function() {
   it('should return a user with valid email', function() {
-    const user = getUserByEmail("user@example.com", testUsers)
+    const user = getUserByEmail("user@example.com", testUsers);
     const expectedOutput = "userRandomID";
 
     console.log(user)
     assert.strictEqual(user, expectedOutput);
   });
+  it("should return undefined if the email was not found in the database", function() {
+    const user = getUserByEmail("wrongemail@example.com", testUsers);
+    const expectedOutput = undefined;
+
+    assert.strictEqual(user, expectedOutput);
+  })
 });
